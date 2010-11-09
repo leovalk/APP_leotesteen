bf9d8ef31e4bc981d6b3793facd2da488743741b
Leo Valk <leo@leovalk.com>
2010-11-09 13:32:25 -0600

    zit er nu wat in

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+APPLeotesteen::Application.config.secret_token = '9c77bc1ba3e5784717a5d213b8c01bab92382aa1c479115ad8956b5e4d38e8ef00cb52a1a2b20a2839575f2804ece2fbddf310c0b5ac0b65fcf52f73aa39820b'
