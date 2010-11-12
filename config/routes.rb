2ce3668d66b07694cd7d936dd640bc95d8ff369b
Leo Valk <leo@leovalk.com>
2010-11-12 17:07:25 -0600

    klaar voor mega actie

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ APPLeotesteen::Application.routes.draw do
   get "pages/about"
   
   get "pages/geheim"
-
+match '/contact', :to => 'pages#contact'
+match '/about', :to => 'pages#about'
+match '/geheim', :to => 'pages#geheim'
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -56,7 +58,7 @@ APPLeotesteen::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => "welcome#index"
+   root :to => "pages#home"
 
   # See how all your routes lay out with "rake routes"
 
