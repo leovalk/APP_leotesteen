857d8d82ae99d361ef310877c87ad366ca67c85a
Leo Valk <leo@leovalk.com>
2010-11-15 14:08:04 -0600

    siging up gedaan

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,10 +4,21 @@ class UsersController < ApplicationController
 @user = User.find(params[:id])
 @title = @user.name
 end
-	
+	def create
+@user = User.new(params[:user])
+if @user.save
+#sign_in @user
+flash[:success] = "Welcome to Leo's eerste Application!"
+redirect_to @user
+else
+@title = "Sign up"
+render 'new'
+end
+end
 	
 	def new
-  	@title = "Sign up"
-  end
+@user = User.new
+@title = "Sign up"
+end
 
 end
