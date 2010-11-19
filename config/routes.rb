6246f4fb0e9e0e88ab530255d36ee82240ba6396
Leo Valk <leo@leovalk.com>
2010-11-19 12:46:40 -0600

    basis gedaan

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 APPLeotesteen::Application.routes.draw do
-  resources :users
-	
-	
+resources :users
+resources :sessions, :only => [:new, :create, :destroy]
+	get "sessions/new"
 
   get "pages/home"
 
@@ -14,6 +14,8 @@ match '/contact', :to => 'pages#contact'
 match '/about', :to => 'pages#about'
 match '/geheim', :to => 'pages#geheim'
 match '/signup', :to => 'users#new'
+match '/signin', :to => 'sessions#new'
+match '/signout', :to => 'sessions#destroy'
 match '/index', :to => 'users#show'
   # The priority is based upon order of creation:
   # first created -> highest priority.
